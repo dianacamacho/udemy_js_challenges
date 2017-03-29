3e054631d22f3bc4cdbfa8d638f0655d06624072
dianacamacho <dcam1901@yahoo.com>
2017-03-29 01:23:20 -0500

    keypress for enter key starter setup

--- udemy_js_sec_6_app/app.js
+++ udemy_js_sec_6_app/app.js
@@ -1,20 +1,46 @@
-// standalone module 1, nothing to do or commnicate with UI controller
+// BUDGET CONTROLLER
 var budgetController = (function() {
 
+
+
 })();
 
-// standalone module 2, nothing to do or commnicate with budget Controller
 
+// UI CONTROLLER
 var UIController = (function() {
   
+
+
 })();
 
-// these two controllers don't know the other exists. but still need way to send info/read data from the UI and have it processed in budget conroller. to do this, can have an app controller
 
-// modules can take arguments, since they are really just function expressions, so can pass the other two modules as arguments so the app controller knows the info from the other two
-
-// app controller
+// GLOBAL APP CONTROLLER
+// central spot for events and will delegate tasks to the other controllers
 
 var controller = (function(budgetCtrl, UICtrl) {
+  var ctrlAddItem = function() {
+    // 1. Get the filed input daya
+
+    // 2. Add the item to the budget controller
+
+    // 3. Add the item to the UI
+
+    // 4. Calc the budget
+
+    // 5. Display the budget on the UI
+    console.log('works');
+  };
+
+  document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+  // Also want to account for keypress, not just click on the button when adding items
+
+  // checking for keyup on global document since keypress can happen anywhere not jsut on any specific element. Browser auto passes the event object to the callback function.
+
+  document.addEventListener('keypress', function(event) {
+    if (event.keyCode === 13 || event.which === 13) {
+      ctrlAddItem();
+    }
+  });
 
 })(budgetController, UIController);
